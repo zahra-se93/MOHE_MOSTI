c778437f8d0c4569e3f6596d55abfe6f566e569f
Zahra SE <zahrasyahidaehwan@gmail.com>
2022-09-13 14:39:50 +0000

    commit

---
+++ 02datatypes.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Datatypes")
+
+	var score int
+	var cityName string
+	var check bool
+	var lifeline float32
+
+	score = 88
+	cityName = "KL"
+	check = true
+	lifeline = 88.88
+
+	fmt.Println(score)
+	fmt.Println(cityName)
+	fmt.Println(check)
+
+	fmt.Println("Score:", score)
+	fmt.Println("City name:", cityName)
+	fmt.Println("Check:", check)
+	fmt.Println("Lifeline:", lifeline)
+	fmt.Println("My score is ", score, ". I live in ", cityName)
+
+	//use format specifier
+	//add backspace \n to adda new line
+	fmt.Printf("My score is %v. I live in %v. \n", score, cityName)
+	fmt.Printf("My lifeline is %v. \n", lifeline)
+
+	//%V format specifier -> get datatype of variable.
+	fmt.Printf("Datatype of lifeline variable is %V \n", lifeline)
+
+	//auto type inference
+	//let go detect the datatype of the variable automatically
+	// use  :=
+	autoScore := "PASS"
+	fmt.Printf("My autoScore is %v. \n", autoScore)
+	fmt.Printf("Datatype of autoScore variable is %V \n", autoScore)
+
+}
---
+++ 03function.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	welcome()
+	addition(5, 3)
+	result := multiply(4, 10)
+	fmt.Println(result)
+    sum, multiple:= maths(4,4)
+	fmt.Println(sum, " and ",multip)
+
+	sumNew, multipleNew:= maths(4,4)
+	fmt.Println(sumNew, " and ",multipleNew)
+}
+
+func mathsNew(i1 int, i2 int) (s int,m int) {
+	s = i1 * i2
+	m = i1 * i2
+}
+
+func maths(i1 int, i2 int) (int,int) {
+	return i1 * i2,i1 * i2,
+}
+func multiply(i1, i2 int) int {
+	return i1 * i2,
+}
+
+func addition(i1 int, i2 int) {
+	fmt.Println(i1 + i2)
+}
+
+func welcome() {
+	fmt.Println("Welcome to functions in Go Lang")
+}
